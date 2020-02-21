4599f88fa396c1a2f7827815219deb445f3ffda7
Chris Galbraith <chris@resknow.co.uk>
2020-02-21 14:48:16 +0000

    Tidy up & create font scale

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,23 @@
+const { DateTime } = require('luxon');
+const pluginRSS = require('@11ty/eleventy-plugin-rss');
+
 module.exports = eleventyConfig => {
-    eleventyConfig.addPassthroughCopy("admin");
-    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPlugin(pluginRSS);
+
+    eleventyConfig.addFilter('htmlDateString', dateObj => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
+            'yyyy-LL-dd'
+        );
+    });
+
+    eleventyConfig.addPassthroughCopy('admin');
+    eleventyConfig.addPassthroughCopy('templates/assets');
+
+    return {
+        htmlTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+        dir: {
+            input: 'templates'
+        }
+    };
 };
